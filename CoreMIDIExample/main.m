27a8cdcd9ebcbeb0d8df408c40a2a0b19381c4fe
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-08-12 08:09:28 +0300

    Initial Commit

---
+++ CoreMIDIExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreMIDIExample
+//
+//  Created by Panayotis Matsinopoulos on 12/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
